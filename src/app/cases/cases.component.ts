0c40f2deb087d92122eb88f97e2b323b03354768
RomanLesovoy <romadesire97@gmail.com>
2020-04-07 22:06:40 +0300

    Updates for cases flow

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Cases } from '../object-models/Cases';
 
 @Component({
   selector: 'app-cases',
@@ -8,9 +9,21 @@ import { ApiService } from '../api.service';
 })
 export class CasesComponent implements OnInit {
 
+  displayedColumns: string[] = ['name', 'age', 'status'];
+  data: Cases[] = [];
+  isLoadingResults = true;
+
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.api.getCases()
+      .subscribe((res: any) => {
+        this.data = res;
+        console.log(this.data, 'Cases');
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err, 'Cases error');
+        this.isLoadingResults = false;
+      });
   }
-
 }
